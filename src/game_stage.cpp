f1315fbcf629c184cd0c968d953a83fd535475df
amiani <amianijohns@gmail.com>
2020-03-25 17:27:19 -0400

    added control

--- src/game_stage.cpp
+++ src/game_stage.cpp
@@ -6,6 +6,8 @@
  #include "components/camera_component.h"
 #include "components/body_component.h"
 #include "components/shape_component.h"
+#include "components/device_control_component.h"
+#include "services/input_service.h"
 //#include "sprite_layers.h"
 #include "game.h"
 #include "title_stage.h"
@@ -28,16 +30,26 @@ void GameStage::init()
   const cp::Float mass = 4;
   const cp::Float radius = 10;
   const cp::Float moment = cp::momentForCircle(mass, 0, radius);
+  const auto body = std::make_shared<cp::Body>(mass, moment);
+  const auto sprite = Sprite()
+    .setImage(getResources(), "large_grey_01.png")
+    .setPivot(Vector2f(.5f, .5f));
 	world->createEntity()
-	  .addComponent(BodyComponent(std::make_shared<cp::Body>(mass, moment)))
-	  .addComponent(ShapeComponent(std::make_shared<cp::CircleShape>(nullptr, radius)))
-	  .addComponent(SpriteComponent(Sprite().setImage(getResources(), "large_grey_01.png"), 0, 1))
+	  .addComponent(BodyComponent(body))
+	  .addComponent(ShapeComponent(std::make_shared<cp::CircleShape>(body, radius)))
+	  .addComponent(SpriteComponent(sprite, 0, 1))
+    .addComponent(DeviceControlComponent(&game.getInputService()->getInput()))
 		.addComponent(CameraComponent(game.getZoom(), Colour4f(0.0f, 0.0f, 0.0f), 1, 0));
+}
+
+void GameStage::onFixedUpdate(Time t)
+{
+	dynamic_cast<HalleyTestGame&>(getGame()).getInputService()->update(t);
+  world->step(TimeLine::FixedUpdate, t);
 }
 
 void GameStage::onVariableUpdate(Time t)
 {
-	dynamic_cast<HalleyTestGame&>(getGame()).getInputService()->update(t);
 	world->step(TimeLine::VariableUpdate, t);
 }
 
