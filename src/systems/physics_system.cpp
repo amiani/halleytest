88e8ad7d0aaacef91bc9758173e65baa02f009ea
amiani <amianijohns@gmail.com>
2020-03-27 16:28:30 -0400

    adding shapes to space

--- src/systems/physics_system.cpp
+++ src/systems/physics_system.cpp
@@ -3,24 +3,26 @@
 
 class PhysicsSystem final : public PhysicsSystemBase<PhysicsSystem> {
 public:
-    void onEntitiesAdded(Halley::Span<BodiesFamily> es) {
-      for (auto &e : es) {
-        space.add(e.body.body);
-      }
+  void onEntitiesAdded(Halley::Span<BodiesFamily> es) {
+    for (auto &e : es) {
+      space.add(e.body.body);
+      space.add(e.shape.shape);
     }
+  }
 
-    void onEntitiesRemoved(Halley::Span<BodiesFamily> es) {
-      for (auto& e : es) {
-        space.remove(e.body.body);
-      }
+  void onEntitiesRemoved(Halley::Span<BodiesFamily> es) {
+    for (auto& e : es) {
+      space.remove(e.body.body);
+      space.remove(e.shape.shape);
     }
+  }
 
-    void update(Halley::Time time) {
-      space.step(time);
-    }
+  void update(Halley::Time time) {
+    space.step(time);
+  }
 
 private:
-    cp::Space space;
+  cp::Space space;
 };
 
 REGISTER_SYSTEM(PhysicsSystem)
