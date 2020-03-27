52fb856df7f89ec22b9741a53d8581e65e7b42e8
amiani <amianijohns@gmail.com>
2020-03-27 15:43:20 -0400

    added asteroid

--- src/game_stage.cpp
+++ src/game_stage.cpp
@@ -3,7 +3,7 @@
 #include "registry.h"
 #include "components/sprite_component.h"
 #include "components/position_component.h" 
- #include "components/camera_component.h"
+#include "components/background_camera_component.h"
 #include "components/body_component.h"
 #include "components/shape_component.h"
 #include "components/device_control_component.h"
@@ -27,19 +27,38 @@ void GameStage::init()
 	world->addService(painterService);
 	world->addService(game.getInputService());
 
-  const cp::Float mass = 4;
-  const cp::Float radius = 10;
+  for (auto& n : getResources().get<ConfigFile>("gameplay/celestial_bodies")->getRoot()["asteroids"].asSequence()) {
+    const auto id = n["id"].asString();
+    const auto mass = n["mass"].asFloat();
+    const auto radius = n["radius"].asFloat();
+    const auto image = n["image"].asString();
+    const cp::Float moment = cp::momentForCircle(mass, 0, radius);
+    const auto body = std::make_shared<cp::Body>(mass, moment);
+    body->setPosition(cp::Vect(0, 100));
+    world->createEntity()
+      .addComponent(BodyComponent(body))
+      .addComponent(ShapeComponent(std::make_shared<cp::CircleShape>(body, radius)))
+      .addComponent(SpriteComponent(Sprite()
+        .setImage(getResources(), image)
+        .setPivot(Vector2f(.5f, .5f))
+        .scaleTo(Vector2f(2*radius, 2*radius)),
+        0, 1));
+  }
+
+  const cp::Float mass = 1;
+  const cp::Float radius = 32;
   const cp::Float moment = cp::momentForCircle(mass, 0, radius);
   const auto body = std::make_shared<cp::Body>(mass, moment);
   const auto sprite = Sprite()
     .setImage(getResources(), "large_grey_01.png")
     .setPivot(Vector2f(.5f, .5f));
+  const auto bgSprite = Sprite().setImage(getResources(), "goldstartile.jpg");
 	world->createEntity()
 	  .addComponent(BodyComponent(body))
 	  .addComponent(ShapeComponent(std::make_shared<cp::CircleShape>(body, radius)))
 	  .addComponent(SpriteComponent(sprite, 0, 1))
     .addComponent(DeviceControlComponent(&game.getInputService()->getInput()))
-		.addComponent(CameraComponent(game.getZoom(), Colour4f(0.0f, 0.0f, 0.0f), 1, 0));
+		.addComponent(BackgroundCameraComponent(game.getZoom(), Colour4f(0.0f, 0.0f, 0.0f), 1, 0, bgSprite));
 }
 
 void GameStage::onFixedUpdate(Time t)
