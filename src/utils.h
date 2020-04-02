f34a31bd73e9bd90a47a39a6484a1b75504185cb
amiani <amianijohns@gmail.com>
2020-04-02 11:50:32 -0400

    add body collision types

--- src/utils.h
+++ src/utils.h
@@ -6,3 +6,11 @@
 Halley::Vector2f chipToScreen(cp::Vect, cp::Vect);
 cp::Vect screenToChip(Halley::Vector2f, cp::Vect);
 Halley::Vector2f chipToHalley(const cp::Vect&);
+
+enum BodyType {
+  PLAYERSHIP,
+  PROJECTILE,
+  ENEMYSHIP,
+  SENSOR,
+  ASTEROID
+};
