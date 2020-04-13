473cadf7d72b1538081a782f45551f0aecbdc106
amiani <amianijohns@gmail.com>
2020-04-13 08:46:06 -0400

    add discount

--- src/control/trainer.cpp
+++ src/control/trainer.cpp
@@ -24,15 +24,15 @@ Policy ActorCritic::improve(Batch& batch) {
   actorOptimizer->zero_grad();
 
   //calculate targets for critic update
-  auto inputs = std::vector<torch::jit::IValue>{batch.observations.to(DEVICE)};
+  auto inputs = std::vector<torch::jit::IValue>{observations};
   auto valuesOld = critic.forward(inputs).toTensor().squeeze(2);
   auto nextValuesOld = valuesOld.index({Slice(), Slice(1,None)});
   nextValuesOld = F::pad(nextValuesOld, { {0, 1} });
-  auto targets = batch.rewards.add(nextValuesOld).detach();
+  auto targets = rewards.add(nextValuesOld).detach();
 
   //update critic with MSE loss
-  criticOptimizer->zero_grad();
   auto loss = torch::mse_loss(valuesOld, targets);
+  std::cout << "critic loss: " << loss << std::endl;
   loss.backward();
   criticOptimizer->step();
 
@@ -40,40 +40,14 @@ Policy ActorCritic::improve(Batch& batch) {
   auto obsValues = critic.forward(inputs).toTensor().squeeze(2);
   auto nextsValues = obsValues.index({Slice(), Slice(1, None)});
   nextsValues = F::pad(nextsValues, { {0, 1} });
-  auto advantages = batch.rewards.add(nextsValues).sub(obsValues);
+  const float GAMMA = .99;
+  auto advantages = rewards.add(GAMMA*nextsValues).sub(obsValues);
 
   //use pseudo-loss with advantages
-  auto pseudoLoss = batch.actionLogProbs.mul(advantages).sum(); //TODO: Use the mean across trajectories when using more than one trajectory!!!
+  auto pseudoLoss = actionLogProbs.mul(advantages).sum(); //TODO: Use the mean across trajectories when using more than one trajectory!!!
 
   //update parameters with gradient of pseudo-loss
-  actorOptimizer->zero_grad();
   pseudoLoss.backward();
   actorOptimizer->step();
   return actor;
 }
-
-/*
-std::tuple<Tensor, Tensor, Tensor, Tensor> ActorCritic::trajectoriesToTensors(std::vector<Trajectory>& trajectories) {
-  std::vector<Tensor> obs;
-  std::vector<Tensor> nexts;
-  std::vector<float> rewards;
-  std::vector<Tensor> logProbs;
-  int N = 0;
-  for (auto& trajectory : trajectories) {
-    for (auto& transition : trajectory) {
-      obs.push_back(transition.observation.toTensor());
-      logProbs.push_back(transition.action.logProb);
-      rewards.push_back(transition.reward);
-      nexts.push_back(transition.next.toTensor());
-
-      N++;
-    }
-  }
-  return {
-    torch::stack(obs),
-    torch::cat(logProbs),
-    torch::from_blob(rewards.data(), N),
-    torch::stack(nexts)
-  };
-}
-*/
