fea9641840242330329b971645baa6add24a38ce
amiani <amianijohns@gmail.com>
2020-04-14 21:34:48 -0400

    shrink model

--- src/control/actor.py
+++ src/control/actor.py
@@ -1,6 +1,6 @@
 import torch
 
-N, D_o, H1, H2, H3, D_a = 1, 6*31, 100, 100, 100, 6
+N, D_o, H1, H2, H3, D_a = 1, 6*31, 100, 50, 100, 4
 
 o = torch.randn(D_o)
 
@@ -9,9 +9,7 @@ model = torch.nn.Sequential(
   torch.nn.ReLU(),
   torch.nn.Linear(H1, H2),
   torch.nn.ReLU(),
-  torch.nn.Linear(H2, H3),
-  torch.nn.ReLU(),
-  torch.nn.Linear(H3, D_a)
+  torch.nn.Linear(H2, D_a)
 )
 
 
--- src/control/critic.py
+++ src/control/critic.py
@@ -1,6 +1,6 @@
 import torch
 
-N, T, obsdim, H1, H2, H3, valuedim = 1, 1, 6*31, 100, 100, 100, 1
+N, T, obsdim, H1, H2, H3, valuedim = 1, 1, 6*31, 100, 50, 100, 1
 
 o = torch.randn(N, T, obsdim)
 
@@ -9,12 +9,8 @@ model = torch.nn.Sequential(
   torch.nn.ReLU(),
   torch.nn.Linear(H1, H2),
   torch.nn.ReLU(),
-  torch.nn.Linear(H2, H3),
-  torch.nn.ReLU(),
-  torch.nn.Linear(H3, valuedim)
+  torch.nn.Linear(H2, valuedim)
 )
-
-model(o)
 
 traced = torch.jit.trace(model, o)
 traced.save('critic.pt')
