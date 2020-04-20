ba17194439f75014ce60fed490d707be6ebd5627
ratang <amaianijohns@gmail.com>
2020-04-20 15:13:43 -0400

    avoid copying data

--- src/control/sac/replay_buffer.cpp
+++ src/control/sac/replay_buffer.cpp
@@ -13,16 +13,16 @@ Batch ReplayBuffer::sample(int size) {
   for (int i = 0; i != size; ++i) {
     auto& traj = buffer[rand() % buffer.size()];
     int stepIndex;
-    Step step;
+    Step* step;
     for (bool terminal = true; terminal;) {
       stepIndex = rand() % traj.size();
-      step = traj[stepIndex];
-      terminal = step.observation.terminal;
+      step = &traj[stepIndex];
+      terminal = step->observation.terminal;
     }
     auto& next = traj[stepIndex+1].observation;
-    o.push_back(step.observation.toTensor());
-    a.push_back(step.action.toTensor());
-    r.push_back(step.reward);
+    o.push_back(step->observation.toTensor());
+    a.push_back(step->action.toTensor());
+    r.push_back(step->reward);
     n.push_back(next.toTensor());
 
     auto reward = torch::from_blob(r.data(), {r.size()});
