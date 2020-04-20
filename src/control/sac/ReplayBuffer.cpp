6c53512d0a623892a9809a0491ef06839eb550fa
ratang <amaianijohns@gmail.com>
2020-04-20 14:07:59 -0400

    add replay buffer

---
+++ src/control/sac/ReplayBuffer.cpp
@@ -0,0 +1,43 @@
+//
+// Created by amiani on 4/20/20.
+//
+
+#include "ReplayBuffer.h"
+
+const Batch& ReplayBuffer::sample(int size) {
+  std::vector<Tensor> o;
+  std::vector<Tensor> a;
+  std::vector<float> r;
+  std::vector<Tensor> n;
+
+  for (int i = 0; i != size; ++i) {
+    auto& traj = buffer[rand() % buffer.size()];
+    int stepIndex;
+    Step step;
+    for (bool terminal = true; terminal;) {
+      stepIndex = rand() % traj.size();
+      step = traj[stepIndex];
+      terminal = step.observation.terminal;
+    }
+    auto& next = traj[stepIndex+1].observation;
+    o.push_back(step.observation.toTensor());
+    a.push_back(step.action.toTensor());
+    r.push_back(step.reward);
+    n.push_back(next.toTensor());
+    return {
+      stack(o),
+      cat(a), //TODO: change this once action tensors are bigger than 1x1
+      from_blob(r.data(), {r.size()}),
+      stack(n)
+    };
+  }
+}
+
+void ReplayBuffer::addStep(Observation o, Action a, float r) {
+  Step step{o, a, r};
+  if (o.terminal) {
+    buffer.push_back(Trajectory{step});
+  } else {
+    buffer.back().push_back(step);
+  }
+}
---
+++ src/control/sac/ReplayBuffer.h
@@ -0,0 +1,34 @@
+//
+// Created by amiani on 4/20/20.
+//
+
+#pragma once
+
+#include "src/control/mdp.h"
+#include "torch/torch.h
+using namespace torch;
+
+struct Step {
+    Observation observation;
+    Action action;
+    float reward;
+};
+
+typedef std::vector<Step> Trajectory;
+
+struct Batch {
+  Tensor observation;
+  Tensor action;
+  Tensor reward;
+  Tensor next;
+};
+
+class ReplayBuffer {
+public:
+  const Batch& sample(int size);
+  void addStep(Observation o, Action a, float r);
+
+private:
+  std::vector<Trajectory> buffer;
+};
+
