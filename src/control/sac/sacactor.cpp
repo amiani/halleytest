03cb1fd8f901df1aac40d583a00961585f7ae09a
ratang <amaianijohns@gmail.com>
2020-04-21 19:39:53 -0400

    get tensor from output

--- src/control/sac/sacactor.cpp
+++ src/control/sac/sacactor.cpp
@@ -7,9 +7,10 @@
 Action SACActor::act(const Observation& o) {
   auto input = o.toTensor().to(DEVICE);
   auto deterministic = torch::zeros({1}).to(DEVICE);
-  auto inputs = std::vector<torch::jit::IValue>{input, deterministic};
-  auto out = module.forward(inputs).toTuple()->elements();
-  auto angle = out[0].toTensor().item<float>();
+  auto out = module.forward({input, deterministic}).toTuple()->elements();
+  auto tensor = out[0].toTensor().detach();
+  auto angle = tensor.item<float>();
+  //std::cout << fmod(angle, 2*M_PI) << std::endl;
   auto target = cp::Vect(cos(angle), sin(angle));
   return {
     true,
