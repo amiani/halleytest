fab59bada409d6e2ef1b7d297cf8a6b4d47b5ad3
ratang <amaianijohns@gmail.com>
2020-04-21 19:39:22 -0400

    make two critics

--- src/control/sac/critic.py
+++ src/control/sac/critic.py
@@ -12,4 +12,12 @@ critic = torch.nn.Sequential(
 
 input = torch.randn(obdim+acdim)
 traced = torch.jit.trace(critic, input)
-traced.save('critic.pt')
+
+def reinit(m):
+    if type(m) == torch.nn.Linear:
+        torch.nn.init.xavier_uniform_(m.weight)
+        torch.nn.init.zeros_(m.bias)
+
+traced.save('critic1.pt')
+critic.apply(reinit)
+traced.save('critic2.pt')
