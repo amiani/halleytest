7a9961fd9852df8f138f78f8566f9a8ab51d17ab
ratang <amaianijohns@gmail.com>
2020-05-01 10:17:13 -0400

    change trainer interface to match new single actor

--- src/control/trainer.h
+++ src/control/trainer.h
@@ -11,9 +11,8 @@ class Trainer {
 public:
   Trainer(){};
   Trainer(std::shared_ptr<Actor>);
-  virtual void addStep(Observation& o, Action& a, float r) =0;
+  virtual void addStep(const Observation& o, const Action& a, float r) =0;
   virtual void improve() =0;
-  virtual std::shared_ptr<Actor> getActor() { return actor; };
 
 protected:
   std::shared_ptr<Actor> actor;
